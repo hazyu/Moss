20cd323529f253e0da4b869d4776e580f6e425ed
Mohammed Salim <renablehots@gmail.com>
2026-04-06 16:44:50 +0400

    Improved development on macos

--- build-dyn.sh
+++ build-dyn.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-g++ -shared -fPIC src-cpp/main.cpp -lSDL2 -o main.so
+# Check if the OS is Darwin (macOS)
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    g++ -shared -fPIC -I /opt/homebrew/Cellar/sdl2/2.32.10/include src-cpp/main.cpp -L /opt/homebrew/Cellar/sdl2/2.32.10/lib -lSDL2 -o main.so
+else
+    g++ -shared -fPIC src-cpp/main.cpp -lSDL2 -o main.so
+fi
+
--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 ./build-dyn.sh;
-./tjs-linux bundle src/main.ts;
-./tjs-linux compile src/main.bundle.js main;
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    ./tjs-darwin bundle src/main.ts;
+    ./tjs-darwin compile src/main.bundle.js main;
+else
+    ./tjs-linux bundle src/main.ts;
+    ./tjs-linux compile src/main.bundle.js main;
+fi
+
 ./main;
 rm main;
 rm main.so
